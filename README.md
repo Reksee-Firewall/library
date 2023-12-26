81ee0d83b8b362ba79ce74372aff73560fb903f0
Reksee <ramonfernando@id.uff.br>
2023-12-26 19:45:22 -0300

    Update: Main Page UI implemented. Grid in progress.

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Library
 
-A small Library app.
+1. All of your book objects are going to be stored in a simple array, so add a function to the script (not the constructor) that can take user’s input and store the new book objects into an array. 
 
-- All of our book objects are going to be stored in a simple array;
+2. Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
 
-- A function to the script (not the constructor) that can take user’s input and store the new book objects into an array.
+3. Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read and anything else you might want. How you decide to display this form is up to you.
+
+4. Add a button on each book’s display to remove the book from the library.
+
+5. Add a button on each book’s display to change its read status.
+
+NOTE: You’re not required to add any type of storage right now. You will have the option to come back to this project later on in the course.
--- main.js
+++ main.js
@@ -1,64 +1,77 @@
-const myLibrary = []; 
+const initializeButton = document.getElementById("initialize");
+const background = document.getElementById("background");
+const frontpage = document.getElementById("frontpage");
 
-function Book(name, author, numberOfPages, isRead) {
-    this.name = name; 
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-    this.isRead = isRead;
-    this.toString = function() {
-        return(`${this.name} by ${this.author}, ${this.numberOfPages}, isRead: ${this.isRead}.`); 
-    };
+function initialize() {
+
+    const rightBorderDeco = document.querySelector("#background img:first-child");
+    const leftBorderDeco = document.querySelector("#background img:nth-child(3)");
+
+    rightBorderDeco.style.animation = "upper-slide-up 1.2s forwards";
+    leftBorderDeco.style.animation = "lower-slide-down 1.2s forwards";
+
+    const cardOne = document.getElementById("first-card");
+    cardOne.style.animation = "slide-out 1.2s ease-in forwards";
+
+    const cardTwo = document.getElementById("second-card");
+    cardTwo.style.animation = "slide-out 1s ease-in forwards";
+
+    const cardThree = document.getElementById("third-card");
+    cardThree.style.animation = "slide-out 0.8s ease-in forwards";
+
+    initializeButton.style.animation = "slide-out 0.8s ease-in forwards";
+    initializeButton.style.cursor = "default";
+
+    setTimeout(openLibrary, 1000); 
 }   
 
-function addBookToLibrary() {
-    let name, author='null', numberOfPages='null', isRead=false;
-    alert(`Enter blank space to stop the execution.`);
-    while(true) {
-        name = prompt(`Insert book's name: `);
-        if (name.trim() === '') break;
-        author = prompt(`Who wrote ${name}? `);
-        numberOfPages = prompt(`How many pages does ${name} have? `);
-        isRead = prompt(`Have you already read it? [yes/no]: `) === 'yes';
+function openLibrary() {
 
-        const newBook = new Book(name, author, numberOfPages, isRead);
-        myLibrary.push(newBook);;
+    // Removes all remaining nodes  
 
-        alert(`"${name}" by ${author} has been added to the Library.`);
+    const backChildNodes = Array.from(background.childNodes);
+    backChildNodes.forEach((child) => {
+        if (child !== background.lastElementChild)
+            background.removeChild(child);
+    });
+    
+    const frontChildNodes = Array.from(frontpage.childNodes);
+    frontChildNodes.forEach((child) => {
+        if (child !== frontpage.lastElementChild)
+            frontpage.removeChild(child);
+    });
+
+    // Student Reading Background 
+    
+    const studentOneIllu = document.createElement("img");
+    studentOneIllu.setAttribute("id", "illu-one"); 
+    studentOneIllu.setAttribute("class", "illu");
+    studentOneIllu.setAttribute("src", "images/Student-reading-a-book-V2.svg");
+    
+    background.appendChild(studentOneIllu); 
+    background.style.height = "100%";
+}
+
+function initializeGrid(gridNumber) {
+    // Books Grid 
+    const booksGrid = document.createElement("div"); 
+    booksGrid.setAttribute("id", "grid");     
+
+    for (i = 1; i <= 2; i++) {
+        for (j = 1; j <= 4; j++) {
+            const gridElement = document.createElement("div"); 
+            gridElement.setAttribute("class", "gridItem");
+            gridElement.setAttribute("id", `G${String(gridNumber).padStart(2, '0')}L${String(i).padStart(2, '0')}C${String(j).padStart(2, '0')}`);
+            booksGrid.appendChild(gridElement);
+        }
     }
-    alert("Library addition operation is over.");
+
+    frontpage.appendChild(booksGrid);
 }
 
-function removeBookFromLibrary() {
-    const number = prompt("What's the index of the book to be removed within the Library?");
-    myLibrary.splice(myLibrary.indexOf(number), 1);
-}
-
-function showBook() {
-    const number = Number(prompt("What's the index of the book to be shown within the Library?"));
-    const numberOfBooks = myLibrary.length;
-    if ((number < 0) || (number > numberOfBooks - 1)) 
-        return ("There's no such book within the Library.");
-    return (myLibrary[number].toString());
-}
-
-function showLibrary() {
-    return myLibrary.toString();
-}  
-
-alert(`Welcome to the Virtual Library! \nEnter blank space to stop execution.`);
-let entry = '';
-while(true) {
-    entry = prompt(`How would you like to proceed? \nAdd [1] | Remove [2] | Show Library [3] | Show Book [4]`);
-    if (entry === "1") {
-        addBookToLibrary();
-    } else if (entry === "2") {
-        removeBookFromLibrary();
-    } else if (entry === "3") {
-        alert(showLibrary());
-    } else if (entry === "4") {
-        alert(showBook());
-    } else {
-        break;
-    }
-}
-alert("Library is closed.");
+initializeButton.addEventListener("click", () => {
+    initialize();
+    setTimeout(() => {
+        initializeGrid(1);
+    }, 1000);
+});
